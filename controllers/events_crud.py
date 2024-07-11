de9dd918d53e9f68f85e28fb7a2b6c5850e8408d
Wasil-P <it.dzemidok@gmai.com>
2024-07-11 21:04:30 +0200

    Added functions for creating and viewing events

--- controllers/events_crud.py
+++ controllers/events_crud.py
@@ -1,6 +1,43 @@
-from models.model import Event
-from models.schemas import Event, EventCreate
 
+
+from models.model import Event
+from models.schemas import User, EventCreate
+
+import datetime
+from pytz import utc
 from sqlalchemy.orm import Session
 from fastapi import HTTPException, Depends
 from starlette.status import HTTP_400_BAD_REQUEST
+
+
+def get_events_all(db: Session, skip: int = 0, limit: int = 10):
+    events = (db.query(Event).
+              filter(Event.event_date >= datetime.datetime.now()).
+              offset(skip).limit(limit).
+              all())
+    return events
+
+
+def get_event_by_id(db: Session, id: int):
+    event = db.query(Event).filter(Event.id == id).first()
+    return event
+
+
+def create_new_event(db: Session, event: EventCreate, user: User):
+    event_date = event.event_date.astimezone(utc)
+    now = datetime.datetime.now().astimezone(utc)
+    if event_date <= now:
+        raise HTTPException(status_code=HTTP_400_BAD_REQUEST
+                            , detail='This date is not valid')
+    db_event = Event(
+        name=event.name,
+        event_date=event_date,
+        place=event.place,
+        about=event.about,
+        author_id=user.id,
+    )
+    db.add(db_event)
+    db.commit()
+    db.refresh(db_event)
+
+    return db_event
--- routers/events_routers.py
+++ routers/events_routers.py
@@ -1,22 +1,26 @@
-from models.schemas import Event, EventCreate, InvitationCreate
+from models.db import get_db
+from .users_routers import manager
+from models.schemas import User, Event, EventCreate, InvitationCreate
+from controllers.events_crud import get_events_all, get_event_by_id, create_new_event
 
 from typing import List
+from sqlalchemy.orm import Session
 from fastapi import APIRouter, Depends, HTTPException, status
-
-
 
 
 router = APIRouter()
 
 
-@router.get("/", response_model=List[Event])
-def get_events_all():
-    pass
+@router.get("/", response_model=List[Event], status_code=200)
+def read_events_all(db: Session = Depends(get_db), skip: int = 0, limit: int = 10):
+    events = get_events_all(db=db, skip=skip, limit=limit)
+    return events
 
 
-@router.get("/{id}", response_model=Event)
-def get_event(id: int):
-    pass
+@router.get("/{id}", response_model=Event, status_code=200)
+def get_event(id: int, db: Session = Depends(get_db)):
+    event = get_event_by_id(db=db, id=id)
+    return event
 
 
 @router.get("/author", response_model=List[Event])
@@ -25,8 +29,9 @@ def get_events_author():
 
 
 @router.post("/author", response_model=EventCreate)
-def create_event():
-    pass
+def create_event(event: EventCreate, db: Session = Depends(get_db), user: User = Depends(manager)):
+    new_event = create_new_event(db=db, event=event, user=user)
+    return new_event
 
 
 @router.get("/author/{id}", response_model=Event)
