149050852209127f081e74d5853ae600e43ff8f3
Wasil-P <it.dzemidok@gmai.com>
2024-08-22 18:12:54 +0200

    Add crontab, edit config

--- celery_sender/config.py
+++ celery_sender/config.py
@@ -1,26 +1,30 @@
 import os
 from celery import Celery
 from dotenv import load_dotenv
+from celery.schedules import crontab
+import celery_sender.tasks
 
 load_dotenv()
 
 
 def make_celery():
-    celery_sender = Celery(
+    celery = Celery(
         'worker',
-        backend=f"redis://{os.getenv('HOST')}:{os.getenv('REDIS_PORT')}/0",
-        broker=f"redis://{os.getenv('HOST')}:{os.getenv('REDIS_PORT')}/0"
+        backend=f"redis://{os.getenv('REDIS_HOST')}:{os.getenv('REDIS_PORT')}/0",
+        broker=f"redis://{os.getenv('REDIS_HOST')}:{os.getenv('REDIS_PORT')}/0"
     )
-    celery_sender.conf.update(
+    celery.conf.update(
         beat_schedule={
             'add-every-30-seconds': {
-                'reminder': 'celery_sender.tasks.event_reminder',
-                'schedule': 30.0
+                'task': 'celery_sender.tasks.event_reminder',
+                'schedule': crontab(hour="8", minute="0")
             },
         },
         timezone='UTC',
     )
-    return celery_sender
+
+    return celery
 
 
-celery_sender = make_celery()
+celery = make_celery()
+
